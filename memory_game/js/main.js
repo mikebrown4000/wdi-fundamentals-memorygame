f41d8f02ce9f772df2c902ee35729addc3b229d0
Michael Brown <michaelsbrown@Michaels-MacBook-Pro.local>
2019-01-14 22:25:46 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,24 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[1];
+var checkForMatch = function(){
 
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+if (cardsInPlay[0] === cardsInPlay[1]){
+	console.log("You found a match!");
+} else {
+	console.log("Sorry, try again!");
+}
+};
+
+var flipCard = function(cardId){
+
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
 
 if (cardsInPlay.length === 2){
-if (cardsInPlay[0] === cardsInPlay[1]){
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again!");
+checkForMatch();
 }
+};
 
-}
+flipCard(0);
+flipCard(2);
